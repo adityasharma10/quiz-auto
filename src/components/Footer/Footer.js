d6316a85235c8cfa3c196e9f9864ed2c6147bc97
aditya <shrmdty123@gmail.com>
2023-09-03 00:56:10 +0530

    1st submission

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with Material UI by{" "}
+      <a
+        href="https://www.linkedin.com/in/aditya-sharma-53013b170/"
+        style={{ cursor: "pointer" }}
+      >
+       Aditya Sharma
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
